b95e1d443d30b5055021a5ac270d4ed1ef529d2b
hamza temsamani <hamzatemsamani1308@gmail.com>
2024-10-21 13:13:01 +0100

    cleaner header file

--- headers/minishell.h
+++ headers/minishell.h
@@ -10,16 +10,16 @@
 
 typedef enum t_token
 {
-	IN,         // <		0
-	OUT,        // >		1
-	HEREDOC,    // <<		2
-	APPEND,     // >>		3
-	AND,        // &&		4
-	PIPE,       // |		5
-	OR,         // ||		6
-	LPR,        // (		7
-	RPR,        // )		8
-	STR,        // string	9
+	IN,         // <		
+	OUT,        // >		
+	HEREDOC,    // <<		
+	APPEND,     // >>		
+	AND,        // &&		
+	PIPE,       // |		
+	OR,         // ||		
+	LPR,        // (		
+	RPR,        // )		
+	STR,        // string	
 	END         // end dyal cmd
 }   s_token;
 
@@ -44,37 +44,37 @@ typedef struct t_input
 	s_redir			*redirections;
 }   s_input;
 
-s_token check_token(char c1, char c2);
-int check_beggining_of_str(char *str);
-void shunting_yard(s_input **head, s_input **tok_stack, s_input **new_stack);
-void push_2(s_input **top, s_input *to_add);
-void push(s_input **a_head, s_input **b_head, int flg);
-s_input *re_order_command(s_input **head);
-s_input *list_to_tree(s_input *root);
-s_input *parsing(char *rl);
-int check_true(s_token tok);
-s_input *my_Tokenizer(char *str);
-int check_rpr(char *s, int i);
-int check_token_syntax(char *s, int *i, int tok, int *par);
-int check_syntax(s_token tok, char *s);
-int check_spaces(char c);
-int check_pre(s_token tok);
-s_input *node_creation_cmd(char *s, s_redir *redir, s_token tok, int token_flag);
-s_redir *node_creat_redir(char **s, s_token tok);
-int build_redir_list(s_redir **head, s_redir *add);
-void incre(char *s, int *i);
-s_input *token_2(char *s, s_token tok, int *i);
-int choose_str(char c, int flg);
-int str_len(char *s, int i, int flg);
-int check_next_quote(char *s, char c);
-void check_flg(int flg, char *s, int *k);
-char *cmd_help(char *s, int l, int *k, int flg);
-char **fill_command(char *s, int l, int *k, int flg);
-char **prep_cmd(char *s, int *i, s_token tok, int flg);
-int token_1(s_input **head, char *s, int *i, int *par);
-int check_left(s_input **head);
-void	remove_top(s_input **tok_s);
-int	build_command_list(s_input **head, s_input *add);
-int	check_syntax_help(s_token tok, s_token next);
+s_token 		check_token(char c1, char c2);
+int 			check_beggining_of_str(char *str);
+void 			shunting_yard(s_input **head, s_input **tok_stack, s_input **new_stack);
+void 			push_2(s_input **top, s_input *to_add);
+void 			push(s_input **a_head, s_input **b_head, int flg);
+s_input 		*re_order_command(s_input **head);
+s_input 		*list_to_tree(s_input *root);
+s_input 		*parsing(char *rl);
+int 			check_true(s_token tok);
+s_input 		*my_Tokenizer(char *str);
+int 			check_rpr(char *s, int i);
+int 			check_token_syntax(char *s, int *i, int tok, int *par);
+int 			check_syntax(s_token tok, char *s);
+int 			check_spaces(char c);
+int 			check_pre(s_token tok);
+s_input 		*node_creation_cmd(char *s, s_redir *redir, s_token tok, int token_flag);
+s_redir 		*node_creat_redir(char **s, s_token tok);
+int 			build_redir_list(s_redir **head, s_redir *add);
+void 			incre(char *s, int *i);
+s_input 		*token_2(char *s, s_token tok, int *i);
+int 			choose_str(char c, int flg);
+int 			str_len(char *s, int i, int flg);
+int 			check_next_quote(char *s, char c);
+void 			check_flg(int flg, char *s, int *k);
+char 			*cmd_help(char *s, int l, int *k, int flg);
+char 			**fill_command(char *s, int l, int *k, int flg);
+char 			**prep_cmd(char *s, int *i, s_token tok, int flg);
+int 			token_1(s_input **head, char *s, int *i, int *par);
+int 			check_left(s_input **head);
+void			remove_top(s_input **tok_s);
+int				build_command_list(s_input **head, s_input *add);
+int				check_syntax_help(s_token tok, s_token next);
 
 #endif
